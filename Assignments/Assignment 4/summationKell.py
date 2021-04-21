948ee9b74fb4290edd71f21be0f1520e469ab6f4
kellymartin127 <79598841+kellymartin127@users.noreply.github.com>
2021-04-21 10:43:00 -0400

    Create summationKell.py

---
+++ Assignments/Assignment 4/summationKell.py
@@ -0,0 +1,19 @@
+for x if range(10):
+
+
+j = 0 
+sum = 0  
+while j < 10: 
+    sum += j 
+    j += 1 
+    print("sum:", sum) 
+    
+    i = 9 
+    while i >= 0: 
+        j = 0 
+        sum = 0  
+        while j < 10: 
+            sum += j 
+            j += 1 
+        print("sum:",sum) 
+        i -= 1
