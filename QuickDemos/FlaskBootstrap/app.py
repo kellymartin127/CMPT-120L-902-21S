d699d5f877f194ce3117654a31b1d24472844bc1
kellymartin127 <79598841+kellymartin127@users.noreply.github.com>
2021-05-26 17:17:56 -0400

    project

--- QuickDemos/FlaskBootstrap/app.py
+++ QuickDemos/FlaskBootstrap/app.py
@@ -1,19 +1,7 @@
-from flask import Flask, render_template
-from random import randint
+from flask import Flask, url_for, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-@app.route('/name')
-def name(name=None):
-    names=["Fred", "Jack", "Ashley", "Angellica"]
-    name=names[randint(0,3)]
-    return render_template("index.html", name=name)
-
-@app.route('/age')
-def age():
-    return 'Hey!' 
-
-
+def index(): 
+    return render_template(index.html)
+    
